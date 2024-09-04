ca561a07037bd2454d7a43720fa7ba4e10dc75a0
wilson <welopez07@gmail.com>
2024-09-04 18:32:46 -0500

    create folders baackend y fronted

---
+++ jaden/src/main/java/com/jaden_detalles/jaden/JadenApplication.java
@@ -0,0 +1,13 @@
+package com.jaden_detalles.jaden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadenApplication.class, args);
+	}
+
+}
---
+++ jaden/src/test/java/com/jaden_detalles/jaden/JadenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaden_detalles.jaden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JadenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
